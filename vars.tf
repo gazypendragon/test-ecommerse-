14479c7b6c311f6b7033e4712fb5356382835598
gazypendragon <96093884+gazypendragon@users.noreply.github.com>
2022-07-03 11:12:19 -0400

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+    default = "s3-demo-19159"
+}
+variable "acl_value" {
+    default = "private"
+}
+variable "region" {
+    default = "us-east-2"
+}
+variable "aws_access_key" {
+    default = "<AKIAZGRWXXLZZHOFPDJW>"
+}
+variable "aws_secret_key" {
+    default = "<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>"
+}
