08b4fec7436b24f155214b420e6a3aa3bd91a880
gazypendragon <96093884+gazypendragon@users.noreply.github.com>
2022-07-07 13:48:41 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -8,8 +8,8 @@ variable "region" {
     default = "us-east-2"
 }
 variable "aws_access_key" {
-    default = "<AKIAZGRWXXLZZHOFPDJW>"
+    default = "AKIAZGRWXXLZZHOFPDJW"
 }
 variable "aws_secret_key" {
-    default = "<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>"
+    default = "NKQwQxgFXEfQq7JhNNR8Xk4Fn0xdXdEsxl7Ne8lR"
 }
