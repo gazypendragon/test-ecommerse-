e75c1ef06b1a67a4058957657b628fd46d4301fc
gazypendragon <96093884+gazypendragon@users.noreply.github.com>
2022-07-08 09:14:52 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,15 +1,9 @@
 variable "bucket_name" {
-    default = "s3-choh-19159"
+    default = "s3-friday-19159"
 }
 variable "acl_value" {
     default = "private"
 }
 variable "region" {
     default = "us-east-2"
-}
-variable "aws_access_key" {
-    default = "AKIAZGRWXXLZZARVICDZ"
-}
-variable "aws_secret_key" {
-    default = "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"
 }
