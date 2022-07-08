d38ed3acdb7b17ef8695488901e37d5a3918f245
gazypendragon <96093884+gazypendragon@users.noreply.github.com>
2022-07-08 09:00:10 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
-  provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+provider "aws" {
   region = "${var.region}"
+  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
 }
