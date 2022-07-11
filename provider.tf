861a18963e88dd6dadc37a7d5e783473496fb844
gazypendragon <96093884+gazypendragon@users.noreply.github.com>
2022-07-10 20:15:44 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,11 @@
+provider "aws" {
+  region = "${var.region}"
+  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
+}
 terraform {
   backend "s3" {
     bucket = "tf-friday-19159"
     key    = "terraform.tstate"
     region = "us-east-2"
   }
-}
-  
-provider "aws" {
-  region = "${var.region}"
-  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
 }
